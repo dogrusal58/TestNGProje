f5ddb1e5e491ab2deadd2af362af14e1c49168d2
dogrusal58 <dogrusalhoca58@gmail.com>
2020-09-08 23:18:15 +0300

    hotelornegi

--- src/test/java/com/techproed/tests/WaitTest.java
+++ src/test/java/com/techproed/tests/WaitTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 public class WaitTest extends TestBase {
     @Test
@@ -12,5 +13,21 @@ public class WaitTest extends TestBase {
         WebElement removeButonu = driver.findElement(By.xpath("//*[.='Remove']"));
         removeButonu.click();
         WebElement element = driver.findElement(By.xpath("//*[.='Add']"));
+        Assert.assertTrue(element.isDisplayed());
+    }
+    @Test
+    public void explicitWait(){
+        driver.get("http://the-internet.herokuapp.com/dynamic_controls");
+        //Explicit Wait kullanmak için, WebDriverWait class'ından nesne üretmek zorundayız.
+        WebDriverWait wait = new WebDriverWait(driver,30);
+
+        WebElement removeButton = driver.findElement(By.xpath("//*[.='Remove']"));
+        removeButton.click();
+
+       // WebElement message = driver.findElement(By.id("message"));
+        //System.out.println(message.getText());
+
+        WebElement message =wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("message")));
+        System.out.println(message.getText());
     }
 }
